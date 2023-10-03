7a1b789e1dbdbadcff46c6150c00e188ebb39dd4
MANUELgp47 <62995481+MANUELgp47@users.noreply.github.com>
2023-10-03 11:10:58 +0200

    commit inicial

---
+++ src/pkg/Empleado.java
@@ -0,0 +1,42 @@
+package pkg;
+
+public class Empleado {
+	
+	public enum TipoEmpleado{Vendedor, Encargado};
+	
+	public float calculoNominaBruta(TipoEmpleado tipo, float ventasMes, float horasExtra) {
+		float salarioBruto;
+		
+		if (tipo == TipoEmpleado.Vendedor)
+			salarioBruto = 2000;
+		else
+			salarioBruto = 2500;
+		
+		if (ventasMes >= 1500)
+			salarioBruto += 200;
+		else if (ventasMes >= 1000)
+			salarioBruto += 100;
+		
+		salarioBruto += horasExtra*30;
+		
+		
+		return salarioBruto;
+		
+	}
+	
+	public float calculaNominaNeta(float nominaBruta) {
+		
+		float nominaNeta;
+		
+		if (nominaBruta < 2100) {
+			nominaNeta= nominaBruta;
+		}else if (nominaBruta < 2500)
+			nominaNeta = (float) (nominaBruta - (nominaBruta * 0.15));
+		else
+			nominaNeta = (float) (nominaBruta - (nominaBruta * 0.18));
+		
+		
+		return nominaNeta;
+	}
+
+}
